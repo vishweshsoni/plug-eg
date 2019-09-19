55b27f5e31f018056e8da22211f9f5bf0d860f9f
vishwesh <vishweshsoni@gmail.com>
2019-09-19 19:41:55 +0530

    Adding License

--- htdocs/wp-content/plugins/plug-eg/plug-eg.php
+++ htdocs/wp-content/plugins/plug-eg/plug-eg.php
@@ -12,3 +12,20 @@
    License:GPL v2 or later
    Text-Domain: plug-eg
  */
+/*
+      This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.if not,write to the free software 
+    Foundation,Inc. 51 Frankstine Street,Fifth Floor,Boston,MA 02110-1301,USA.
+    
+    Copyright 2005-2015 Automattic Inc.
+ */
