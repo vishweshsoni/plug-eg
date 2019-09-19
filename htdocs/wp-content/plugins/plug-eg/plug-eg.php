a544e60aa4ceaf6744de63a27101e1f34b9cbfd2
vishwesh <vishweshsoni@gmail.com>
2019-09-19 19:17:27 +0530

    adding plugin info

--- htdocs/wp-content/plugins/plug-eg/plug-eg.php
+++ htdocs/wp-content/plugins/plug-eg/plug-eg.php
@@ -3,6 +3,12 @@
  *  @package plug-eg
  */
 /**
- plugin name: plug-eg
-  
+   Plugin Name: plug-eg
+   Plugin URI: https://github.com/vishweshsoni/plug-eg 
+   Description: My First Attempt to writing custom plugin
+   version: 1.0.0
+   Author: Vishwesh Soni
+   Author URI: https://github.com/vishweshsoni
+   License:GPL v2 or later
+   Text-Domain: plug-eg
  */
