1e6a44df33cdb2aa84b6662b0a6d22e51ebeac07
vishwesh <vishweshsoni@gmail.com>
2019-09-19 22:05:28 +0530

    added antother method for deleting database

--- htdocs/wp-content/plugins/plug-eg/unistall.php
+++ htdocs/wp-content/plugins/plug-eg/unistall.php
@@ -9,8 +9,17 @@ if(!defined('WP_UNINSTALL_PLUGIN')){
         die;
 }
 
+// Method 1 using for each loop
 //clear database store data
 $books= get_post( array('post_type'=>'book','numberofposts'=>-1));
 foreach($books as $val){
         wp_delete_post( $val->ID,true); 
 }
+
+
+//method 2 using sql
+//access the database via sql
+// global $wpdb;
+// $wpdb->query("DELETE FROM wp_posts WHERE post_type='book'");
+// $wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id from wp_posts)");
+// $wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN(SELECT id FROM wp_posts)");
